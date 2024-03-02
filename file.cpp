d6fb3e541dc34a2010700e77d2e65e11972621c6
Brax Wong <ghostenjob@gmail.com>
2024-03-02 17:07:34 +0000

    Able to add content to a file without repeated content.

--- file.cpp
+++ file.cpp
@@ -8,6 +8,7 @@ class File{
   public:
     std::vector<std::string> readFileData(std::string fileName);
     void writeToFile(std::vector<std::string> fileData, std::string fileName);
+    void appendToFile(std::string fileName);
 };
 
 std::vector<std::string> File::readFileData(std::string fileName)
@@ -28,5 +29,32 @@ void File::writeToFile(std::vector<std::string> fileData, std::string fileName)
   for(int i = 0; i < fileData.size(); ++i)
   {
     file << fileData[i] << "\n";
+  }
+}
+
+void File::appendToFile(std::string fileName)
+{
+  while(true)
+  {
+    std::vector<std::string> fileData = readFileData(fileName);
+    std::cout << "Please enter the content you would like to append to the file.\n";
+    std::string content;
+    std::getline(std::cin, content);
+    for(int i = 0;i < fileData.size(); ++i)
+    {
+      if(fileData[i] == content)
+      {
+        std::cout << "The content already exists within the file, please try again.\n";
+        break;
+      }
+    }
+    std::ofstream file(fileName, std::ios_base::app);
+    file << content << "\n";
+    std::cout << "Would you like to continue(Y/N)\n";
+    std::getline(std::cin, content);
+    if(content == "N" || content == "n")
+    {
+      break;
+    }
   }
 }
--- main.cpp
+++ main.cpp
@@ -1,38 +1,46 @@
 #include "encodeText.cpp"
 #include "decodeText.cpp"
+#include "file.cpp"
 #include <iostream>
 #include <stdio.h>
 #include <vector>
 #include <limits>
 
+std::string getFileName()
+{
+  std::string fileName;
+  std::cout << "Please provide the name of the file:\n";
+  std::getline(std::cin, fileName);
+  return fileName;
+}
 int main()
 {
   std::cout << "Welcome to the Text Encryption Program.\n";
   while(1)
   {
     int num;
-    std::string fileName;
-    std::cout << "Encryption(Press 1): \nDecryption(Press 2):\nQuit The Program(Press 3):\n";
+    std::cout << "Encryption(Press 1):\nDecryption(Press 2):\nAdd Content To File(Press 3):\nQuit The Program(Press 4):\n";
     std::cin >> num; 
     std::cin.clear();
     std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
     if(num==1)
     {
       Encoder encoder;
-      std::cout << "Please provide the name of the file:\n";
-      std::getline(std::cin, fileName);
-      encoder.startEncryption(fileName);
+      encoder.startEncryption(getFileName());
       std::cout << "Encryption Complete...\n";
     }
     else if(num == 2)
     {
-      std::cout << "Please provide the name of the file:\n";
-      std::getline(std::cin, fileName);
       Decoder decoder;
-      decoder.decryption(fileName);
+      decoder.decryption(getFileName());
       std::cout << "Decryption Complete...\n";
     }
     else if(num == 3)
+    {
+      File file;
+      file.appendToFile(getFileName());
+    }
+    else if(num == 4)
     {
       break;
     }
